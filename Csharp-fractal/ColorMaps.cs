25e7c542626b53210aabe378ec46a54467e9a28a
lkct <lkct@yahoo.com>
2019-12-16 21:17:04 +0800

    julia

---
+++ Csharp-fractal/ColorMaps.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+
+namespace Csharp_fractal
+{
+    public delegate Color ColorMap(double norm, int iter);
+    public delegate int Double2IntColor(double v, double coef);
+
+    static class ColorMaps
+    {
+        public static ColorMap GetCmap(double[] bg_rgb, double[] fg_rgb, Double2IntColor d2i)
+        {
+            return (norm, iter) =>
+            {
+                if (norm < Julia.R)
+                {
+                    return Color.Black;
+                }
+
+                double v = Math.Log(iter - Math.Log(Math.Log(norm, 2), 2) + 2, 2) / 4;
+                int r, g, b;
+                if (v < 1.0)
+                {
+                    double conv = Math.Max(0, v); // 收敛率
+                    r = d2i(conv, bg_rgb[0]);
+                    g = d2i(conv, bg_rgb[1]);
+                    b = d2i(conv, bg_rgb[2]);
+                }
+                else
+                {
+                    double div = Math.Max(0, 2 - v);
+                    r = d2i(div, fg_rgb[0]); // 发散率
+                    g = d2i(div, fg_rgb[1]);
+                    b = d2i(div, fg_rgb[2]);
+                }
+                return Color.FromArgb(r, g, b);
+            };
+        }
+    }
+}
---
+++ Csharp-fractal/Julia.cs
@@ -0,0 +1,49 @@
+﻿using System.Drawing;
+using System.Numerics;
+
+namespace Csharp_fractal
+{
+    struct ImgRegion
+    {
+        public double Left { get; set; }
+        public double Right { get; set; }
+        public double Down { get; set; }
+        public double Up { get; set; }
+        public double Step { get; set; }
+    }
+
+    static class Julia
+    {
+        public static double R { get; set; } = 4.0;
+        public static int MaxIter { get; set; } = 100;
+
+        public static Bitmap GetJulia(ImgRegion region, Complex c, ColorMap cmap)
+        {
+            int width = (int)((region.Right - region.Left) / region.Step);
+            int height = (int)((region.Up - region.Down) / region.Step);
+            Bitmap img = new Bitmap(width, height);
+
+            int i, j;
+            double x, y;
+            for (i = 0, x = region.Left; i < width; i++, x += region.Step)
+            {
+                for (j = 0, y = region.Down; j < height; j++, y += region.Step)
+                {
+                    Complex z = new Complex(x, y);
+                    int iters;
+                    for (iters = 0; iters < MaxIter; iters++)
+                    {
+                        if (z.Magnitude >= R)
+                        {
+                            break;
+                        }
+                        z = z * z + c;
+                    }
+                    img.SetPixel(i, j, cmap(z.Magnitude, iters));
+                }
+            }
+
+            return img;
+        }
+    }
+}
